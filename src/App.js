7021e5f9ffad802eeb5a6d1a02a834ba0ecce5db
Juan Jose Figueroa Hurtatis <juanjosefigueroapromwork@gmail.com>
2020-11-30 22:27:25 -0500

    Consulta la Api y leyendo resultados

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 function App() {
   const[categoria, guardarCategoria] = useState('');
+  const[noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=dbfacc9e6d674fe7a1ece49c8cf46dc5`
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria]);
+
   return (
     <Fragment>
       <Header titulo='Buscador de Noticias' />
