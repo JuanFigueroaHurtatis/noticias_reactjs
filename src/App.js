42dd107ff5cd1eed0ccfca54decaf9d2b4f1564b
Juan Jose Figueroa Hurtatis <juanjosefigueroapromwork@gmail.com>
2020-11-30 20:32:48 -0500

    Creacion de formulario y modulo de css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
-import Header from './components/Header'
+import Header from './components/Header';
+import Formulario from './components/Formulario';
 function App() {
   return (
     <Fragment>
-      <Header titulo='Buscador de Noticias'/>
+      <Header titulo='Buscador de Noticias' />
       <div className="container white">
-        <h1>Formulario Aqui</h1>
+        <Formulario />
       </div>
     </Fragment>
   );
